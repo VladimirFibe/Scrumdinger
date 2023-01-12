9f628b2a48daeb5a4475f5c1fa4c6f8dd69cd227
fibeapp <motiw@me.com>
2023-01-12 19:35:23 +0600

    Updating app data

--- Scrumdinger/ScrumsView.swift
+++ Scrumdinger/ScrumsView.swift
@@ -2,6 +2,8 @@ import SwiftUI
 
 struct ScrumsView: View {
     @Binding var scrums: [DailyScrum]
+    @State private var isPresentingNewScrumView = false
+    @State private var newScrumData = DailyScrum.Data()
     var body: some View {
         List {
             ForEach($scrums) { $scrum in
@@ -14,11 +16,29 @@ struct ScrumsView: View {
         .navigationTitle("Daily Scrums")
         .toolbar {
             Button(action: {
-                print("DEBUG: \(#function)")
+                isPresentingNewScrumView = true
             }) {
                 Image(systemName: "plus")
             }
             .accessibilityLabel("New Scrum")
+        }
+        .sheet(isPresented: $isPresentingNewScrumView) {
+            NavigationView {
+                DetailEditView(data: $newScrumData)
+                    .toolbar {
+                        ToolbarItem(placement: .cancellationAction) {
+                            Button("Dismiss") { isPresentingNewScrumView = false }
+                        }
+                        ToolbarItem(placement: .confirmationAction) {
+                            Button("Add") {
+                                let newScrum = DailyScrum(data: newScrumData)
+                                scrums.append(newScrum)
+                                isPresentingNewScrumView = false
+                                newScrumData = DailyScrum.Data()
+                            }
+                        }
+                    }
+            }
         }
     }
 }
